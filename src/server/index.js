2d4b2155888d1d5387da042d8d680b88ab2067d5
joey493 <ebrahimnader321@gmail.com>
2021-11-30 22:01:20 +0200

    added Offline Functionality

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,9 @@ dotenv.config()
 const apiKey = process.env.API_KEY
 
 app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
+
 })
 
 // designates what port the app will listen to for incoming requests
